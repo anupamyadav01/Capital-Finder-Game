7e6c5bad46a499f0c706480a3ec473db33614016
Anupam Yadav <anupamy571@gmail.com>
2024-12-04 09:45:41 +0530

    Tailwind Config Modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,6 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// };
-
-// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       animation: {
